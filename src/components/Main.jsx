7f0b03dfb87b9d08684589a64c2291815061e08a
Reuben <reubenthomas99@gmail.com>
2023-01-23 19:43:19 +0530

    Add css styles

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 function Main() {
     return (
-        <div>
+        <section className="wrapper">
+        <div className="component">
             <div>
-                <img src={require("../images/image-qr-code.png")}></img>
+                <img className="qr-code" src={require("../images/image-qr-code.png")}></img>
             </div>
             <div>
                 <h1>Improve your front-end skills by building projects</h1>
                 <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
             </div>
         </div>
+        </section>
     );
 }
 
