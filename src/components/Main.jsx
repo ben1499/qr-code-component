0265c484c12aa87cf05dd1f3499a47c201a7f7dc
Reuben <reubenthomas99@gmail.com>
2023-01-26 09:28:30 +0530

    Add accessibility changes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,15 +3,15 @@ import React from "react";
 function Main() {
     return (
         <section className="wrapper">
-        <div className="component">
+        <main className="component">
             <div>
-                <img className="qr-code" src={require("../images/image-qr-code.png")}></img>
+                <img className="qr-code" alt="QR code to frontendmentor.io" src={require("../images/image-qr-code.png")}></img>
             </div>
             <div>
                 <h1>Improve your front-end skills by building projects</h1>
                 <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
             </div>
-        </div>
+        </main>
         </section>
     );
 }
